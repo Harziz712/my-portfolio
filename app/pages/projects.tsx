ccd6a52b687a0d3d092e90a0ef974316bff245de
Akanbi AbdulAzeez <akanbiazeez117@gmail.com>
2025-04-17 08:30:12 +0100

    fix: project section responsiveness

--- app/pages/projects.tsx
+++ app/pages/projects.tsx
@@ -33,56 +33,20 @@ const Projects = () => {
    </div>
       <GlassShadow />
 
-      {/* <Carousel
-        plugins={[autoplayPlugin.current]}
-        className="w-full max-w-sm md:max-w-max py-10  border-2 border-amber-400"
-        onLoadStart={autoplayPlugin.current.stop}
-        onMouseLeave={autoplayPlugin.current.reset}
-      >
-        <CarouselContent className="ml-1 md:ml-0 md:w-full h-full py-10 "> */}
-          {/* {testimonials.map(({ id, imageUrl, name, position, testimonial }) => (
-            <CarouselItem key={id} className="pl-1 md:basis-1/2 lg:basis-1/3">
-              <div  className="p-1">
-                <Glassmorphism className="relative justif</div>y-center items-center h-[300px] p-6 border-none shadow-2xl rounded-2xl bg-accent">
-                  <div className="absolute left-1/2 z-20 -top-6 -translate-x-1/2 w-20 h-12">
-                    <Image
-                      src={imageUrl}
-                      alt={name}
-                      style={{
-                        left: '32%',
-                        zIndex: 10,
-                        objectFit: 'contain',
-                        position: 'absolute',
-                      }}
-                      className="rounded-full shadow-md"
-                    />
-                  </div>
-                  <div className="pt-20 text-center h-[200px]">
-                    <h3 className="text-lg font-bold text-primary">{name}</h3>
-                    <p className="text-sm text-gray-500">{position}</p>
-                    <p className="italic text-gray-700 mt-3">{testimonial}</p>
-                  </div>
-                </Glassmorphism>
-                
-              </div>
-            </CarouselItem>
-          ))} */}
-            <div className=" px-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 w-full pt-10 ">
+
+            <div className=" px-12 md:px-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 w-full pt-10 ">
            {ProjectList.map((project, id) => (
                 <Glassmorphism key={id} className="w-[300px]  p-0 mt-4 rounded-2xl  shadow-md  overflow-hidden">
                   <GlassShadow/>
           
                 <div data-aos="zoom-in-down"
                 data-aos-easing="ease-in-cubic" className="flex flex-col items-center ">
-              {/* <div className="w-full h-[150px] overflow-hidden rounded-2xl"> */}
                 <Image
                   src={project.image}
                   alt="project1"
                   className="w-full h-full object-cover "
                 />
-              {/* </div> */}
-          
-              {/* Text Content */}
+ 
           
               <div className="w-full px-4 py-3 inset-1 rounded-2xl  text-center space-y-2 flex flex-col items-center ">
                 <h3 className=" font-semibold text-lg text-primary uppercase">{project.title}</h3>
@@ -95,10 +59,7 @@ const Projects = () => {
           
             ))}
             </div>
-        {/* </CarouselContent>
-        <CarouselPrevious className="bg-primary" />
-        <CarouselNext className="bg-primary" />
-      </Carousel> */}
+
     </div>
   );
 };
